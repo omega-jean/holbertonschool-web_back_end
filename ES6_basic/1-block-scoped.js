f83c2aef54b2c3b15bc17969ab272f2171fb2fe6
omega-jean <jeanalex1405@gmail.com>
2024-07-17 11:50:01 +0200

    test

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-    task = false;
-    task2 = true;
+    let task = false;
+    let task2 = true;
   
     if (trueOrFalse) {
         task = true;
