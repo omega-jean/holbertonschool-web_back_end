79b71f0dd1fbc9079b52561ed265b46c898e8e28
omega-jean <jeanalex1405@gmail.com>
2024-07-18 14:01:25 +0200

    task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const NewArray = []
+
+    for (const idx of array) {
+        NewArray.push(appendString + idx);
+    }
+    return NewArray;
+}
