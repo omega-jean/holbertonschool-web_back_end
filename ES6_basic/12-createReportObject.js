ca6ca1443c71c68ce12050fa229619c6ad8f1353
omega-jean <jeanalex1405@gmail.com>
2024-07-18 14:48:30 +0200

    modify

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,6 +2,5 @@ export default function createReportObject(employeesList) {
     return {
         allEmployees: { ...employeesList },
         getNumberOfDepartments: (employees) => Object.keys(employees).length,
-        }
     };
 }
