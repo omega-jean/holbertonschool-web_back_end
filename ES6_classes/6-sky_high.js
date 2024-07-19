0dd7c3092cf5ba062b49a5cee84c347d1ce32b8f
omega-jean <jeanalex1405@gmail.com>
2024-07-19 15:29:53 +0200

    task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,34 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this.floors = floors;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Sqft must be a number');
+        }
+        this._sqft = value;
+    }
+
+    get floors() {
+        return this._floors;
+    }
+
+    set floors(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Floors must be a number');
+        }
+        this._floors = value;
+    }
+
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}
