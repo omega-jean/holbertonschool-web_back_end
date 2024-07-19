9ea1959c6d5a61e8d912957a436252fd1e5ae7aa
omega-jean <jeanalex1405@gmail.com>
2024-07-19 14:31:05 +0200

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+    constructor(name, lenght, students) {
+        this.name = name
+        this.lenght = lenght
+        this.students = students
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = value;
+    }
+
+    get lenght(){
+        return this._lenght;
+    }
+
+    set lenght(value) {
+        if (typeof value !== 'Number') {
+            throw new TypeError('Lenght must be a number')
+        }
+        this._lenght = value;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(value) {
+        if (!Array.isArray(value) || !students.every(value => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = value
+    }
+}
