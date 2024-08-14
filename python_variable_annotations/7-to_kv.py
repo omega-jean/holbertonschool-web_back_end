bf372ca30276fdf6a3090c8a73683ff905712df3
omega-jean <jeanalex1405@gmail.com>
2024-08-14 14:28:06 +0200

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""to_kv"""
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """to_kv"""
+    x = v ** 2
+    return (k, x)
