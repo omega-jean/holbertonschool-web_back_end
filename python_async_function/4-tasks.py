9ead3b71e569dedbab371512ef556d5ab239c877
omega-jean <jeanalex1405@gmail.com>
2024-08-16 14:30:40 +0200

    task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""task_wait"""
+
+from typing import List
+import asyncio
+import random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """task_wait"""
+    spawn_list = []
+    delay_list = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
+
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
