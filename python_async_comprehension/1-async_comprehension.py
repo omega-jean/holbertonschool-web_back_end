4cdb8ba309c87946559c46948d1e658fb3d0de5e
omega-jean <jeanalex1405@gmail.com>
2024-08-19 14:16:26 +0200

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""from the previous task and then write a coroutine called
-async_comprehension that takes no arguments."""
+"""async_comprehension"""
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
-    """from the previous task and then write a coroutine called
-    async_comprehension that takes no arguments."""
+    """async_comprehension"""
     return [num async for num in async_generator()]
