806442774d83fa62d4bc8dbdf866d51e34281d6e
omega-jean <jeanalex1405@gmail.com>
2024-08-23 15:33:38 +0200

    fix pycodestyle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-""" 12-main """
+"""Python script that provides some stats
+about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
+
 def main():
+    """Python script that provides some stats about
+    Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
@@ -17,8 +21,11 @@ def main():
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check_count = collection.count_documents({
+        "method": "GET", "path": "/status"
+    })
     print(f"{status_check_count} status check")
+
 
 if __name__ == "__main__":
     main()
