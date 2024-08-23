1772a79b3b8aa9f05dca035908ede8dfa1d0e30c
omega-jean <jeanalex1405@gmail.com>
2024-08-23 10:35:16 +0200

    trying a way to typeannote

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async_generator"""
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """async_generator"""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
